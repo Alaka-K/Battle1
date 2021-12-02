430128bb1a53f848a8f83fe92de2ccc95aa1c9cf
kehinde alaka <kennyalaka93@gmail.com>
2021-12-02 19:57:35 +0000

    /play route in progress

--- spec/app.rb
+++ spec/app.rb
@@ -1,6 +1,5 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
-require './lib/player'
 
 class Battle < Sinatra::Base
   configure :development do
@@ -18,20 +17,15 @@ class Battle < Sinatra::Base
 
   post '/names' do
     p params
-   
-     p @Name1 = Player.new(params[:Name1])
-    p @Name2 = Player.new(params[:Name2])
-   erb :play 
+   session[:Name1] = params[:Name1]
+   session[:Name2] = params[:Name2]
+   redirect '/play'
   end
   
   get '/play' do
-  #  p 
-  #   @Name1 = Player.new(params[:Name1])
-  #   @Name2 = Player.new(params[:Name2])
-    
-  #   # @Name1 = session[:Name1]
-  #   # @Name2 = session[:Name2]
-  #   erb :play
+    @Name1 = session[:Name1]
+    @Name2 = session[:Name2]
+    erb :play
   end
 
   get '/attack' do
--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -2,6 +2,6 @@ feature 'attack player 2' do
     scenario "attack" do
       sign_in_and_play
       click_button 'attack'
-      expect(page).to have_content 'Player 1 Attacked Player 2'
+      expect(page).to have_content 'Attacked Player 2'
     end 
   end
